324d4cf61693f3cabf07999242aefce6beacffb4
ray2408 <63856754+ray2408@users.noreply.github.com>
2026-05-04 11:29:38 +0530

    4may

---
+++ DAY 4/q4.cpp
@@ -0,0 +1,22 @@
+//hollow square//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r;
+    cout<<"enter no of rows and columns";
+    cin>>r;
+
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=r;j++){
+            if(i==1||i==r||j==1||j==r){
+                cout<<" *";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+    cout<<endl;
+    }
+}
