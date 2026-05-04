a921f16a8d4ea4e6753fa7cc4f4466884f53d3a2
ray2408 <63856754+ray2408@users.noreply.github.com>
2026-05-04 09:56:44 +0530

    4may

---
+++ DAY 4/q2.cpp
@@ -0,0 +1,16 @@
+//cascading pattern//
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r;
+    cout<<"enter no of rows";
+    cin>>r;
+
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+    cout<<endl;
+    }
+}
