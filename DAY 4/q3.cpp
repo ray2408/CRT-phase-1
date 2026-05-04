31147b4a30493e1b83172cd9cd5c311c8d438739
ray2408 <63856754+ray2408@users.noreply.github.com>
2026-05-04 10:28:00 +0530

    4may

--- DAY 4/q3.cpp
+++ DAY 4/q3.cpp
@@ -1,17 +1,20 @@
-//pattern//
+//mirrored triangle pattern//
+
 #include <iostream>
 using namespace std;
 
 int main(){
-    int r;
+    int r,c;
     cout<<"enter no of rows";
     cin>>r;
 
     for(int i=1;i<=r;i++){
-        for(int j=1;j<=i;j++){
+        for(int j=1;j<=r-i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=i;k++){
             cout<<"*";
         }
     cout<<endl;
     }
-    return 0;
 }
