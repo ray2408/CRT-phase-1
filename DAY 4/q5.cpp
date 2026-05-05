f93cf87743c77400346469a5195a6aba33e60338
ray2408 <63856754+ray2408@users.noreply.github.com>
2026-05-05 09:14:44 +0530

    com

---
+++ DAY 4/q5.cpp
@@ -0,0 +1,21 @@
+//pyramid pattern//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r,c;
+    cout<<"enter no of rows";
+    cin>>r;
+
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=r-i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=i;k++){
+            cout<<" *";
+        }
+    cout<<endl;
+    }
+}
+
