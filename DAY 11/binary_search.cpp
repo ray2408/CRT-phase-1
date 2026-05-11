cbf10c6f201b93a25d497ccfbd641f567f92bf8b
ray2408 <63856754+ray2408@users.noreply.github.com>
2026-05-11 10:49:39 +0530

    commit

---
+++ DAY 11/binary_search.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int binary_search(int nums[], int n, int target){
+    int left=0;
+    int right=n-1;
+
+    while(left<=right){
+        int mid= left+(right-left)/2;
+
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]>target){
+            right=mid-1;
+        }
+        else{
+            left=mid+1;
+        }
+    }
+    return -1;//element not found
+}
+
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int size= sizeof(arr)/sizeof(arr[0]);
+
+    int ans= binary_search(arr,size,3);
+    cout<<"element found at index:"<< ans<< endl;
+
+    return 0;
+}
